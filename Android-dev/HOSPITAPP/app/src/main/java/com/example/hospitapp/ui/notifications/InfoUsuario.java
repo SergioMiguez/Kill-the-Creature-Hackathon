1b96009a6e59c861a376920cfd0629b83788bb8d
Antonio León Villares <antonioleonvillares@Antonios-MacBook-Air.local>
2020-04-05 02:50:27 +0200

    Failed at implementing saving user preferences

--- Android-dev/HOSPITAPP/app/src/main/java/com/example/hospitapp/ui/notifications/InfoUsuario.java
+++ Android-dev/HOSPITAPP/app/src/main/java/com/example/hospitapp/ui/notifications/InfoUsuario.java
@@ -1,13 +1,11 @@
 package com.example.hospitapp.ui.notifications;
 
 import android.content.Intent;
-import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import com.example.hospitapp.R;
 
@@ -22,33 +20,34 @@ public class InfoUsuario extends AppCompatActivity {
         saveUserBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 EditText nameEdited = findViewById(R.id.nameEdited);
-                EditText nameHospitalEdited = findViewById(R.id.nameHospitalEdited);
-                EditText nameStreetEdited = findViewById(R.id.nameStreetEdited);
-                EditText streetNumberEdited = findViewById(R.id.streetNumberEdited);
-                EditText CPEdited = findViewById(R.id.CPEdited);
-                EditText cityEdited = findViewById(R.id.cityEdited);
-                EditText emailEdited = findViewById(R.id.emailEdited);
-                EditText telephoneEdited = findViewById(R.id.telephoneEdited);
+                //EditText nameHospitalEdited = findViewById(R.id.nameHospitalEdited);
+                //EditText nameStreetEdited = findViewById(R.id.nameStreetEdited);
+                //EditText streetNumberEdited = findViewById(R.id.streetNumberEdited);
+                //EditText CPEdited = findViewById(R.id.CPEdited);
+                //EditText cityEdited = findViewById(R.id.cityEdited);
+                //EditText emailEdited = findViewById(R.id.emailEdited);
+                //EditText telephoneEdited = findViewById(R.id.telephoneEdited);
 
                 String nameEditedStr = nameEdited.getText().toString();
-                String nameHospitalEditedStr = nameHospitalEdited.getText().toString();
-                String nameStreetEditedStr = nameStreetEdited.getText().toString();
-                String streetNumberEditedStr = streetNumberEdited.getText().toString();
-                String CPEditedStr = CPEdited.getText().toString();
-                String cityEditedStr = cityEdited.getText().toString();
-                String emailEditedStr = emailEdited.getText().toString();
-                String telephoneEditedStr = telephoneEdited.getText().toString();
+                //String nameHospitalEditedStr = nameHospitalEdited.getText().toString();
+                //String nameStreetEditedStr = nameStreetEdited.getText().toString();
+                //String streetNumberEditedStr = streetNumberEdited.getText().toString();
+                //String CPEditedStr = CPEdited.getText().toString();
+                //String cityEditedStr = cityEdited.getText().toString();
+                //String emailEditedStr = emailEdited.getText().toString();
+                //String telephoneEditedStr = telephoneEdited.getText().toString();
 
                 Intent updateUserInfo = new Intent(InfoUsuario.this, NotificationsFragment.class);
                 updateUserInfo.putExtra("key.name.edited", nameEditedStr);
-                updateUserInfo.putExtra("key.name.hospital.edited", nameHospitalEditedStr);
-                updateUserInfo.putExtra("key.name.street.edited", nameStreetEditedStr);
-                updateUserInfo.putExtra("key.street.number.edited", streetNumberEditedStr);
-                updateUserInfo.putExtra("key.CP.edited", CPEditedStr);
-                updateUserInfo.putExtra("key.city.edited", cityEditedStr);
-                updateUserInfo.putExtra("key.email.edited", emailEditedStr);
-                updateUserInfo.putExtra("key.telephone.edited", streetNumberEditedStr);
+                //updateUserInfo.putExtra("key.name.hospital.edited", nameHospitalEditedStr);
+                //updateUserInfo.putExtra("key.name.street.edited", nameStreetEditedStr);
+                //updateUserInfo.putExtra("key.street.number.edited", streetNumberEditedStr);
+                //updateUserInfo.putExtra("key.CP.edited", CPEditedStr);
+                //updateUserInfo.putExtra("key.city.edited", cityEditedStr);
+                //updateUserInfo.putExtra("key.email.edited", emailEditedStr);
+                //updateUserInfo.putExtra("key.telephone.edited", telephoneEditedStr);
                 startActivity(updateUserInfo);
 
             }
