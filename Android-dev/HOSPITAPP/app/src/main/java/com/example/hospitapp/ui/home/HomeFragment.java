243db7f91ae6d3c1599d4268d66eec2701147f66
Sergio <miguezsergio22@gmail.com>
2020-04-05 21:11:23 +0200

    Server Call Get List

--- Android-dev/HOSPITAPP/app/src/main/java/com/example/hospitapp/ui/home/HomeFragment.java
+++ Android-dev/HOSPITAPP/app/src/main/java/com/example/hospitapp/ui/home/HomeFragment.java
@@ -1,21 +1,28 @@
 package com.example.hospitapp.ui.home;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
 import android.widget.ListAdapter;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.hospitapp.InfoPedidosDialog;
+import com.example.hospitapp.MainActivity;
 import com.example.hospitapp.Order;
 import com.example.hospitapp.R;
 import com.example.hospitapp.ui.ListClassAdapter;
@@ -31,11 +38,12 @@ public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
 
+    private final String state = "Pendientes";
+
     RecyclerView recyclerView;
     ArrayList<Order> listOfOrders;
 
     RequestQueue requestQueue;
-
 
 
 
@@ -46,12 +54,13 @@ public class HomeFragment extends Fragment {
 
         listOfOrders = new ArrayList<>();
 
+
         recyclerView = (RecyclerView) view.findViewById(R.id.recyclerHome);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
         fillList();
 
-        ListClassAdapter adapter = new ListClassAdapter(listOfOrders, "PENDIENTES");
+        ListClassAdapter adapter = new ListClassAdapter(listOfOrders, state);
 
         adapter.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -75,12 +84,12 @@ public class HomeFragment extends Fragment {
         listOfOrders.add(new Order(1,1,20,15, 1, "5/04/2020", "tu casa"));
 
 
-        //makeListRequest("http://URLCALL");
+        makeListRequest("http://URLCALL");
 
     }
 
-    /*
-    private List<Order> makeListRequest (String URL) {
+
+    private void makeListRequest (String URL) {
         StringRequest stringRequest = new StringRequest(Request.Method.GET, URL, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
@@ -90,16 +99,55 @@ public class HomeFragment extends Fragment {
                     for (int i = 0; i < array.length(); i++) {
                         JSONObject order = array.getJSONObject(i);
 
-                        // listOfOrders.add(new Order( order.getInt("id_objeto"), order.getInt("cantidad"), "Pending", order.getInt("id"));
+                        listOfOrders.add(new Order(
+                                order.getInt("id"),
+                                order.getInt("id_objeto"),
+                                order.getInt("cantidad"),
+                                order.getInt("id_proveedor"),
+                                order.getInt("id_hospital"),
+                                order.getString("fecha"),
+                                order.getString("direccion_envio")
+                        ));
 
                     }
+
+                    ListClassAdapter adapter = new ListClassAdapter(listOfOrders, state);
+                    recyclerView.setAdapter(adapter);
 
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
             }
-        })
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                if (mContext != null) {
+                    Toast.makeText(mContext, error.toString(), Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+        /*  TODO CHECK IF CONTEXT WORKS  (mContext) */
+        if (mContext != null) {
+            Volley.newRequestQueue(mContext).add(stringRequest);
+        }
+
     }
 
-     */
+    private Context mContext;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mContext = context;
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mContext = null;
+    }
+
+
 }
