da3ffa0ee7fc1c095c5bd8b075ff318f3139c1f0
Antonio León Villares <antonioleonvillares@Antonios-MacBook-Air.local>
2020-04-06 01:44:23 +0200

    Attempted to creater user register-profile-server connection

--- Android-dev/HOSPITAPP/app/src/main/java/com/example/hospitapp/ui/notifications/NotificationsFragment.java
+++ Android-dev/HOSPITAPP/app/src/main/java/com/example/hospitapp/ui/notifications/NotificationsFragment.java
@@ -1,7 +1,6 @@
 package com.example.hospitapp.ui.notifications;
 
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -15,7 +14,25 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.hospitapp.LoginActivity;
+import com.example.hospitapp.Order;
 import com.example.hospitapp.R;
+import com.example.hospitapp.UsuarioHospital;
+import com.example.hospitapp.ui.ListClassAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class NotificationsFragment extends Fragment {
 
@@ -37,9 +54,24 @@ public class NotificationsFragment extends Fragment {
     TextView emailAdded;
     TextView telephoneAdded;
 
+    String defName;
+    String defHospital;
+    String defStreet;
+    String defNumber;
+    String defCP;
+    String defCity;
+    String defEmail;
+    String defTelephone;
+
     SharedPreferences myPrefs;
 
     String def = "ERROR";
+
+    String serverURL = "http::/URL.com";
+
+    UsuarioHospital userHospital;
+
+    private Context mContext;
 
     private NotificationsViewModel notificationsViewModel;
 
@@ -48,6 +80,17 @@ public class NotificationsFragment extends Fragment {
         notificationsViewModel =
                 ViewModelProviders.of(this).get(NotificationsViewModel.class);
         final View root = inflater.inflate(R.layout.fragment_notifications, container, false);
+
+        makeUserRequest(serverURL);
+
+        defName = userHospital.getUsuario();
+        defHospital = userHospital.getNombre();
+        defStreet = userHospital.getDireccion();
+        defNumber = userHospital.getNumberAddress();
+        defCP = userHospital.getZipCode();
+        defCity = userHospital.getCity();
+        defEmail = userHospital.getEmail();
+        defTelephone = userHospital.getTelefono();
 
         nameAdded = root.findViewById(R.id.nameAdded);
         nameHospitalAdded = root.findViewById(R.id.nameHospitalAdded);
@@ -60,14 +103,14 @@ public class NotificationsFragment extends Fragment {
 
         myPrefs = getActivity().getSharedPreferences("prefID", Context.MODE_PRIVATE);
 
-        String name = myPrefs.getString("name", def);
-        String hospital = myPrefs.getString("hospital", def);
-        String street = myPrefs.getString("street", def);
-        String street_number = myPrefs.getString("street_number", def);
-        String CP = myPrefs.getString("CP", def);
-        String city = myPrefs.getString("city", def);
-        String email = myPrefs.getString("email", def);
-        String telephone = myPrefs.getString("telephone", def);
+        String name = myPrefs.getString("name", defName);
+        String hospital = myPrefs.getString("hospital", defHospital);
+        String street = myPrefs.getString("street", defStreet);
+        String street_number = myPrefs.getString("street_number", defNumber);
+        String CP = myPrefs.getString("CP", defCP);
+        String city = myPrefs.getString("city", defCity);
+        String email = myPrefs.getString("email", defEmail);
+        String telephone = myPrefs.getString("telephone", defTelephone);
 
         nameAdded.setText(name);
         nameHospitalAdded.setText(hospital);
@@ -144,24 +187,68 @@ public class NotificationsFragment extends Fragment {
                     cityEdited.getText().clear();
                     emailEdited.getText().clear();
                     telephoneEdited.getText().clear();
-                }
-                else {
+
+                    Toast.makeText(getContext(), "Datos de perfil actualizados correctamente!", Toast.LENGTH_SHORT).show();
+                } else {
                     Toast.makeText(getContext(), "Rellene todos los campos para continuar!", Toast.LENGTH_LONG).show();
                 }
             }
         });
-
         return root;
     }
 
     private boolean fullEdit(EditText editText) {
         String empty = "";
-        return !editText.getText().toString().trim().equals("");
+        return !editText.getText().toString().trim().equals(empty);
     }
 
+    private void makeUserRequest(String URL) {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject user = new JSONObject(response);
 
+                    String[] directionInfo = user.getString("direccion").split("\\Q$\\E");
 
+                    userHospital = new UsuarioHospital(
+                            user.getInt("id"),
+                            user.getString("nombre"),
+                            user.getString("usuario"),
+                            directionInfo[0], //street name
+                            user.getString("email"),
+                            user.getString("telefono"),
+                            directionInfo[1], //street number
+                            directionInfo[2], //zip code
+                            directionInfo[3] //city/province
+                    );
 
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                if (mContext != null) {
+                    Toast.makeText(mContext, "ERROR USER", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }) {
+            // not used yet...
+            @Override
+            public Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> parameters = new HashMap<String, String>();
+                parameters.put("usuario", LoginActivity.userName);
+                return parameters;
+            }
+        };
 
+        /*  TODO CHECK IF CONTEXT WORKS  (mContext) */
+        if (mContext != null) {
+            Volley.newRequestQueue(mContext).add(stringRequest);
+        }
 
+    }
 }
+            
