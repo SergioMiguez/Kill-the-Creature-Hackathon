4f253384f5f92aa230c05aa292e4b0239f634d95
Antonio León Villares <antonioleonvillares@Antonios-MacBook-Air.local>
2020-04-05 11:11:19 +0200

    Failed at everything

--- Android-dev/HOSPITAPP/app/src/main/java/com/example/hospitapp/ui/notifications/InfoUsuario.java
+++ Android-dev/HOSPITAPP/app/src/main/java/com/example/hospitapp/ui/notifications/InfoUsuario.java
@@ -1,13 +1,24 @@
 package com.example.hospitapp.ui.notifications;
 
+import android.app.FragmentTransaction;
+import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.example.hospitapp.MainActivity;
 import com.example.hospitapp.R;
+
+import java.util.zip.Inflater;
 
 public class InfoUsuario extends AppCompatActivity {
 
@@ -22,36 +33,39 @@ public class InfoUsuario extends AppCompatActivity {
             public void onClick(View v) {
 
                 EditText nameEdited = findViewById(R.id.nameEdited);
-                //EditText nameHospitalEdited = findViewById(R.id.nameHospitalEdited);
-                //EditText nameStreetEdited = findViewById(R.id.nameStreetEdited);
-                //EditText streetNumberEdited = findViewById(R.id.streetNumberEdited);
-                //EditText CPEdited = findViewById(R.id.CPEdited);
-                //EditText cityEdited = findViewById(R.id.cityEdited);
-                //EditText emailEdited = findViewById(R.id.emailEdited);
-                //EditText telephoneEdited = findViewById(R.id.telephoneEdited);
+                EditText nameHospitalEdited = findViewById(R.id.nameHospitalEdited);
+                EditText nameStreetEdited = findViewById(R.id.nameStreetEdited);
+                EditText streetNumberEdited = findViewById(R.id.streetNumberEdited);
+                EditText CPEdited = findViewById(R.id.CPEdited);
+                EditText cityEdited = findViewById(R.id.cityEdited);
+                EditText emailEdited = findViewById(R.id.emailEdited);
+                EditText telephoneEdited = findViewById(R.id.telephoneEdited);
 
                 String nameEditedStr = nameEdited.getText().toString();
-                //String nameHospitalEditedStr = nameHospitalEdited.getText().toString();
-                //String nameStreetEditedStr = nameStreetEdited.getText().toString();
-                //String streetNumberEditedStr = streetNumberEdited.getText().toString();
-                //String CPEditedStr = CPEdited.getText().toString();
-                //String cityEditedStr = cityEdited.getText().toString();
-                //String emailEditedStr = emailEdited.getText().toString();
-                //String telephoneEditedStr = telephoneEdited.getText().toString();
+                String nameHospitalEditedStr = nameHospitalEdited.getText().toString();
+                String nameStreetEditedStr = nameStreetEdited.getText().toString();
+                String streetNumberEditedStr = streetNumberEdited.getText().toString();
+                String CPEditedStr = CPEdited.getText().toString();
+                String cityEditedStr = cityEdited.getText().toString();
+                String emailEditedStr = emailEdited.getText().toString();
+                String telephoneEditedStr = telephoneEdited.getText().toString();
 
-                Intent updateUserInfo = new Intent(InfoUsuario.this, NotificationsFragment.class);
+                Intent updateUserInfo = new Intent(InfoUsuario.this, MainActivity.class);
                 updateUserInfo.putExtra("key.name.edited", nameEditedStr);
-                //updateUserInfo.putExtra("key.name.hospital.edited", nameHospitalEditedStr);
-                //updateUserInfo.putExtra("key.name.street.edited", nameStreetEditedStr);
-                //updateUserInfo.putExtra("key.street.number.edited", streetNumberEditedStr);
-                //updateUserInfo.putExtra("key.CP.edited", CPEditedStr);
-                //updateUserInfo.putExtra("key.city.edited", cityEditedStr);
-                //updateUserInfo.putExtra("key.email.edited", emailEditedStr);
-                //updateUserInfo.putExtra("key.telephone.edited", telephoneEditedStr);
+                updateUserInfo.putExtra("key.name.hospital.edited", nameHospitalEditedStr);
+                updateUserInfo.putExtra("key.name.street.edited", nameStreetEditedStr);
+                updateUserInfo.putExtra("key.street.number.edited", streetNumberEditedStr);
+                updateUserInfo.putExtra("key.CP.edited", CPEditedStr);
+                updateUserInfo.putExtra("key.city.edited", cityEditedStr);
+                updateUserInfo.putExtra("key.email.edited", emailEditedStr);
+                updateUserInfo.putExtra("key.telephone.edited", telephoneEditedStr);
                 startActivity(updateUserInfo);
 
             }
         });
 
     }
+
 }
+
+
--- Android-dev/HOSPITAPP/app/src/main/java/com/example/hospitapp/ui/notifications/NotificationsFragment.java
+++ Android-dev/HOSPITAPP/app/src/main/java/com/example/hospitapp/ui/notifications/NotificationsFragment.java
@@ -29,7 +29,6 @@ public class NotificationsFragment extends Fragment {
                 textView.setText(s);
             }
         });
-
          */
 
         Button editUserBtn = root.findViewById(R.id.editUserBtn);
@@ -38,51 +37,14 @@ public class NotificationsFragment extends Fragment {
             public void onClick(View v) {
                 Intent userInfo = new Intent(getContext(), InfoUsuario.class);
                 startActivity(userInfo);
+                try {
+                    finalize();
+                } catch (Throwable throwable) {
+                    throwable.printStackTrace();
+                }
             }
         });
 
-        TextView nameAdded = root.findViewById(R.id.nameAdded);
-        TextView nameHospitalAdded = root.findViewById(R.id.nameHospitalAdded);
-        TextView nameStreetAdded = root.findViewById(R.id.nameStreetAdded);
-        TextView streetNumberAdded = root.findViewById(R.id.streetNumberAdded);
-        TextView CPAdded = root.findViewById(R.id.CPAdded);
-        TextView cityAdded = root.findViewById(R.id.cityAdded);
-        TextView emailAdded = root.findViewById(R.id.emailAdded);
-        TextView telephoneAdded = root.findViewById(R.id.telephoneAdded);
-
-
-        if (getActivity().getIntent().hasExtra("key.name.edited")) {
-            String nameAddedStr = getActivity().getIntent().getStringExtra("key.name.edited");
-            nameAdded.setText(nameAddedStr);
-        }
-        else if (getActivity().getIntent().hasExtra("key.name.hospital.edited")) {
-            String nameHospitalAddedStr = getActivity().getIntent().getStringExtra("key.name.hospital.edited");
-            nameHospitalAdded.setText(nameHospitalAddedStr);
-        }
-        else if (getActivity().getIntent().hasExtra("key.name.street.edited")) {
-            String nameStreetAddedStr = getActivity().getIntent().getStringExtra("key.name.street.edited");
-            nameStreetAdded.setText(nameStreetAddedStr);
-        }
-        else if (getActivity().getIntent().hasExtra("key.street.number.edited")) {
-            String streetNumberStr = getActivity().getIntent().getStringExtra("key.street.number.edited");
-            streetNumberAdded.setText(streetNumberStr);
-        }
-        else if (getActivity().getIntent().hasExtra("key.CP.edited")) {
-            String CPAddedStr = getActivity().getIntent().getStringExtra("key.CP.edited");
-            CPAdded.setText(CPAddedStr);
-        }
-        else if (getActivity().getIntent().hasExtra("key.city.edited")) {
-            String cityAddedStr = getActivity().getIntent().getStringExtra("key.city.edited");
-            cityAdded.setText(cityAddedStr);
-        }
-        else if (getActivity().getIntent().hasExtra("key.email.edited")) {
-            String emailAddedStr = getActivity().getIntent().getStringExtra("key.email.edited");
-            emailAdded.setText(emailAddedStr);
-        }
-        else if (getActivity().getIntent().hasExtra("key.telephone.edited")) {
-            String telephoneAddedStr = getActivity().getIntent().getStringExtra("key.telephone.edited");
-            telephoneAdded.setText(telephoneAddedStr);
-        }
 
         return root;
     }
