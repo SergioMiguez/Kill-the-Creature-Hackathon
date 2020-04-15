a422bcab71c3079702f2b095e6bd9678b1406ff0
Sergio <miguezsergio22@gmail.com>
2020-04-16 00:00:09 +0200

    Materials list display implementation

--- Android-dev/HOSPITAPP/app/src/main/java/com/example/hospitapp/MaterialsDialog.java
+++ Android-dev/HOSPITAPP/app/src/main/java/com/example/hospitapp/MaterialsDialog.java
@@ -5,15 +5,58 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatDialogFragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.hospitapp.ui.ListAdaptor;
+import com.example.hospitapp.ui.ListClassAdapter;
+import com.example.hospitapp.ui.ListMaterialsAdaptor;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MaterialsDialog extends AppCompatDialogFragment {
 
+    private RecyclerView recyclerView;
+    private ArrayList<Material> listOfMaterials;
+    private String state;
+    private RequestQueue requestQueue;
+
+
+
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = getActivity().getLayoutInflater();
         final View view = inflater.inflate(R.layout.materials_dialog, null);
+
+        state = "MATERIALS";
+
+        recyclerView = view.findViewById(R.id.recyclerMaterials);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        listOfMaterials = new ArrayList<>();
+
+        fillList();
+
+        ListMaterialsAdaptor adapter = new ListMaterialsAdaptor(listOfMaterials);
+
+        recyclerView.setAdapter(adapter);
+
         builder.setView(view)
                 .setTitle("Hacer pedido")
                 .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
@@ -28,6 +71,68 @@ public class MaterialsDialog extends AppCompatDialogFragment {
 
                     }
                 });
+
+
         return builder.create();
     }
+
+    private void fillList() {
+        /*  TODO IMPLEMENT URL  */
+        makeListRequest("URL");
+    }
+
+    private void makeListRequest(String URL) {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONArray array = new JSONArray(response);
+                    for (int i = 0; i < array.length(); i++) {
+                        JSONObject material = array.getJSONObject(i);
+
+                        listOfMaterials.add(new Material(
+                                material.getInt("id"),
+                                material.getString("nombre")
+                        ));
+
+                    }
+
+                    ListMaterialsAdaptor adapter = new ListMaterialsAdaptor(listOfMaterials);
+
+                    recyclerView.setAdapter(adapter);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                if (MainActivity.getContext() != null) {
+                    Toast.makeText(MainActivity.getContext(), "ERROR HOME", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        }) {
+            @Override
+            public Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> parameters = new HashMap<String, String>();
+                parameters.put("usuario", LoginActivity.userName);
+                return parameters;
+            }
+        };
+
+        /**
+         *
+         *if (mContext != null) {
+         *Volley.newRequestQueue(mContext).add(stringRequest);
+         *}
+         */
+
+        requestQueue= Volley.newRequestQueue(MainActivity.getContext());
+        requestQueue.add(stringRequest);
+    }
+
+
+
 }
